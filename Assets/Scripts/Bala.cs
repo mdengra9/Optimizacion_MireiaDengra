dc56507a6c18a1ffe527e32ead7bde9fa6d2d0b1
mdengra9 <mireia.den.esp@gmail.com>
2024-02-09 09:30:02 +0100

    thrt

---
+++ Assets/Scripts/Bala.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * bulletSpeed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    public static PoolManager Instance;
+    [SerializeField] string parentName;
+    [SerializeField] GameObject prefab;
+    [SerializeField] int poolSize;
+    [SerializeField] List<GameObject> pooledObject;
+
+    void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject parent = new GameObject(parentName);
+        GameObject obj;
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            obj = Instantiate(prefab);
+            obj.transform.SetParent(parent.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
